0b925adc581a224c2add322f362e1265c64e3f8e
Patrick Lippert <patrickdlippert@gmail.com>
2021-07-17 01:15:31 -0700

    Pinky's UI Fist Commit

---
+++ shared/categories.js
@@ -0,0 +1,9 @@
+const categories = [
+  {id: '1', name: 'scoops', image: require('../assets/images/categories/scoop.png')},
+  {id: '2', name: 'sundaes', image: require('../assets/images/categories/sundae.png')},
+  {id: '3', name: 'shakes', image: require('../assets/images/categories/shake.png')},
+  {id: '4', name: 'cookies', image: require('../assets/images/categories/cookie.png')},
+  {id: '5', name: 'brownies', image: require('../assets/images/categories/brownie.png')}
+];
+
+export default categories;
---
+++ shared/colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+  white: '#FFF',
+  dark: '#000',
+  primary: '#FF66C4', /* '#F9813A' */
+  secondary: '#FFD7F0', /*'#fedac5' */
+  light: '#E5E5E5',
+  grey: '#908E8C',
+};
+
+export default COLORS;
